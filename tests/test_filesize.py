85b76c684e795e5ba638d6f978b6d4495dcca606
rebeccabillings <rbillings@mozilla.com>
2017-06-09 10:45:45 -0700

    Feat: Add test for maximum filesize

---
+++ tests/test_filesize.py
@@ -0,0 +1,15 @@
+import os
+from os.path import getsize
+
+
+dir = ('safebrowsing')
+filenames = []
+for file in os.listdir(dir):
+        if os.path.isfile(os.path.join(dir, file)) and 'mozstd-trackwhite' in file:
+            size = os.path.getsize(os.path.join(dir, file))
+            if size <= 200000:
+                filenames.append(file)
+            else:
+                print file + ' is over maximum filesize'
+print filenames
+print ' are under maximum filesize'
