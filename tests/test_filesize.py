ceb18ed940feaec332af61c87a6f4727d7f6f955
rebeccabillings <rbillings@mozilla.com>
2017-06-27 11:11:14 -0700

    Updated print statements for python3

--- tests/test_filesize.py
+++ tests/test_filesize.py
@@ -5,11 +5,12 @@ from os.path import getsize
 dir = ('safebrowsing')
 filenames = []
 for file in os.listdir(dir):
-        if os.path.isfile(os.path.join(dir, file)) and 'mozstd-trackwhite' in file:
+        if os.path.isfile(os.path.join(dir, file)) and 'mozstd-trackwhite' in file: # noqa
             size = os.path.getsize(os.path.join(dir, file))
             if size <= 200000:
+                assertTrue size <= 200000
                 filenames.append(file)
             else:
-                print file + ' is over maximum filesize'
+                print file + (' is over maximum filesize')
 print filenames
-print ' are under maximum filesize'
+print (' are under maximum filesize')
