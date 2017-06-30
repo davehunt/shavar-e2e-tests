c71f99adffcb21c4b481f4c3ef20e1a107f860ec
rebeccabillings <rbillings@mozilla.com>
2017-06-30 09:38:32 -0700

    Adding test_filesize, initial draft of safebrowsing file cache test

--- tests/test_filesize.py
+++ tests/test_filesize.py
@@ -1,16 +1,44 @@
-import os
-from os.path import getsize
+# This test should only test filesizes when the file exists
+# TODO: check setup to make sure to verify that all the files shavar should
+# have delivered to the cache are actually there.
 
+# 1. get list of files from /safebrowsing
+# 2. get filesizes of ^^
+# 3. add filename/size key pairs to dict
+# 4. get prefs header [ex: whitelist], compare to files in dict and verify size is under maximum
+
+
+import ConfigParser
+import os
+
+
+parser = ConfigParser.SafeConfigParser()
+parser.read('prefs.ini')
 
 dir = ('safebrowsing')
-filenames = []
-for file in os.listdir(dir):
-        if os.path.isfile(os.path.join(dir, file)) and 'mozstd-trackwhite' in file: # noqa
-            size = os.path.getsize(os.path.join(dir, file))
-            if size <= 200000:
-                assert size <= 200000
-                filenames.append(file)
-            else:
-                print file + (' is over maximum filesize')
-print filenames
-print (' are under maximum filesize')
+sections = ['whitelist', 'blacklist', 'content', 'DNT', 'plugin']
+
+p = []
+f = []
+
+def prefs_group(section):
+    # Go through each of the non-default prefs sections and list the files
+    prefs = parser.get(section, 'file_list')
+    p.extend(prefs.split(','))
+    print(p)
+    return p
+for section in sections:
+    prefs_group(section)
+
+def test_safebrowsing():
+    """Hardcoded location of safebrowsing directory will need to be updated
+    to reflect new FF profile file directory"""
+    # Get list of files
+    for name in os.listdir('safebrowsing'):
+        filename = os.path.join('safebrowsing', name)
+        if os.path.isfile(filename):
+            f.append(filename)
+    # Create list with filename, size tuples
+    for i in xrange(len(f)):
+        f[i] = (os.path.splitext(f[i])[0], os.path.getsize(f[i]))
+    return f
