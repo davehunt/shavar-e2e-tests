17aff931ee9ac6280e5e664f96201328d5f16cef
Richard Pappalardo <rpappalax@gmail.com>
2017-08-23 14:53:57 -0700

    feat: add new conf helper functions (WIP)

--- helper_prefs.py
+++ helper_prefs.py
@@ -1,16 +1,29 @@
 from selenium import webdriver
 
+
 dir = ('safebrowsing')
 sections = ['whitelist', 'blacklist', 'content', 'DNT', 'plugin']
 
 p = []
 pf = []
 
+def sections_list_all(conf):
+    return conf.sections()
+
+def section_list_all(conf, name_section):
+    return conf.items(name_section)
+
+# returns expected "prefs-list" list from prefs.ini
+def pref_set_file_list(conf, name_section):
+    return conf.get(name_section, 'file_list').split(',')
 
 def pref_sets_list_all(conf):
-    pass
+    return conf.get('index', 'pref_sets_index').split(',')
 
-def pref_set(name_pref_set):
+def max_file_size_list_all(conf):
+    return conf.get('index', 'max_file_size').split(',')
+
+def pref_set(conf, name_pref_set):
     pass
 
 def pref_set_files_in_safebrowsing_dir(conf, section):
@@ -26,11 +39,6 @@ def pref_set_files_in_safebrowsing_dir(conf, section):
     return pf
 
 
-def prefs_group(conf, section):
-    # Go through each of the non-default prefs sections and list the files
-    prefs = conf.get(section, 'file_list')
-    p.extend(prefs.split(','))
-    return p
 
     for section in sections:
         prefs_group(section)
@@ -44,3 +52,20 @@ def set_prefs(conf, sections):
             print (key, val)
             fp.set_preference(key, val)
     return fp
+
+
+if __name__ == '__main__':
+    import ConfigParser
+ 
+    def conf():
+	config = ConfigParser.ConfigParser()
+	config.read('./prefs.ini')
+	return config
+    conf = conf()
+
+    #val = sections_list_all(conf)
+    #val = section_list_all(conf, 'DNT')
+    #val = pref_set_file_list(conf, 'DNT')
+    #val = pref_sets_list_all(conf)
+    val = max_file_size_list_all(conf)
+    print(val)
